814782e43d88c644f4315edfc461d92158a65a8f
Ronald <hallo@ronaldvanheugten.nl>
2023-04-24 14:22:20 +0200

    init index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+header('HTTP/1.0 403 Forbidden');
+?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<title>van Heugten ICT - 403 - Helaas geen toegang!</title>
+		<meta name="description" content="Deze ruimte is op dit moment niet toegankelijk voor de meeste bezoekers. Probeer het later nog eens!" />
+		<meta name="author" content="van Heugten ICT" />
+
+		<!-- Social -->
+		<meta property="og:site_name" content="van Heugten ICT" />
+		<meta property="og:title" content="van Heugten ICT - 403 - Helaas geen toegang!" />
+		<meta property="og:description" content="Deze ruimte is op dit moment niet toegankelijk voor de meeste bezoekers. Probeer het later nog eens!" />
+		  
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Styles -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" href="/vhict/vhict.css">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+		  
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+	</head>
+
+	<body>
+		<section class="supend-main">
+			<div class="container-fluid">
+				<div class="row text-center">
+					<div class="col-12 col-lg-6 supend-left">
+						<div class="supend-logo-wrapper">
+							<img class="supend-logo" src="/vhict/logo.png"/>
+						</div>
+					</div>
+
+					<div class="col-12 col-lg-6 supend-right">
+
+						<h4 class="mt-3">403 - Helaas geen toegang!</h4>
+						<p>Deze ruimte is op dit moment niet toegankelijk voor de meeste bezoekers. Probeer het later nog eens!</p>
+
+						<p>Met vriendelijke groet,<br/>
+						Team van Heugten ICT</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
